ed99e689bd6842e3ac989b675278915f03c2b71d
Mario Jorquera <mariojz@gmail.com>
2016-05-17 00:06:55 -0300

    chore: browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
+ browserSync.init({
+     server: "./"
+ });
+ browserSync.stream();
 
 gulp.task('styles', function(){
   gulp.src('sass/**/*.scss')
