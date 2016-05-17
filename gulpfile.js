752a34e2b05001ce7b3c4defab2bdb8a038877f6
Mario Jorquera <mariojz@gmail.com>
2016-05-17 00:04:00 -0300

    feat: bootrap initial template + gulp configuration

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('styles', function(){
+  gulp.src('sass/**/*.scss')
+          .pipe(sass().on('error', sass.logError))
+          .pipe(autoprefixer({
+          			browsers: ['last 2 versions'],
+          			cascade: false
+          		}))
+          .pipe(gulp.dest('./css'));
+});
+
+gulp.task('default', function(){
+  gulp.watch('sass/**/*.scss',['styles'])
+});
