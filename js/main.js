6aaa2c4783467e7002b59bf696cf9ce76b16cd5d
Mario Jorquera <mariojz@gmail.com>
2016-05-19 01:55:58 -0300

    feat: signup with notify

--- js/main.js
+++ js/main.js
@@ -1,3 +1,49 @@
-﻿$(document).ready(function() {
-   
+$(document).ready(function() {
+
+  $("#password").popover({
+        placement: "bottom",
+        content: "It's so simple to create a tooltop for my website!" })
+        .on("click focus",function(){
+            $(this).popover('show');
+        })
+        .blur(function () {
+            $(this).popover('hide');
+        });
+
+  function notifySuccess(){
+    $.notify(
+      { icon: 'glyphicon glyphicon-ok-circle',
+        message: "Success! User Created."
+        },{
+       type: 'success',
+       placement: {
+          from: "top",
+          align: "center"
+        },
+        animate: {
+      		enter: 'animated slideInDown',
+      		exit: 'animated slideOutUp'
+      	}
+      });
+  }
+
+  function getFormData(unindexed_array){
+        var indexed_array = {};
+
+        $.map(unindexed_array, function(n, i){
+            indexed_array[n['name']] = n['value'];
+        });
+
+        return indexed_array;
+      }
+
+  $("#sigup-form").on("submit", function(e){
+    var user = getFormData($(this).serializeArray());
+    console.log(user);
+    e.preventDefault();
+    firebase.database().ref('users/').push(user);
+    notifySuccess();
+    $(this).trigger("reset");
+    return true;
+  });
 })
